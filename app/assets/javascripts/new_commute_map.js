61fe767458213e24b8698c2247fb84ab529e1a65
Leon Lukashevsky <leon.lukashevsky@gmail.com>
2015-10-03 13:36:08 -0400

    post route_steps and populate db

--- app/assets/javascripts/new_commute_map.js
+++ app/assets/javascripts/new_commute_map.js
@@ -22,8 +22,39 @@ function calculateAndDisplayRoute(directionsService, directionsDisplay) {
   }, function(response, status) {
     if (status === google.maps.DirectionsStatus.OK) {
       directionsDisplay.setDirections(response);
+      steps = response['routes'][0]['legs'][0]['steps'];
+      ss = simplifyGoogleRouteSteps(steps);
+      ssj = JSON.stringify(ss);
+      postRouteSteps(ssj);
     } else {
       window.alert('Directions request failed due to ' + status);
     }
   });
 }
+
+function simplifyGoogleRouteSteps(steps) {
+  var ss = steps.map(function(step) {
+    return {
+      start: step['start_point'],
+      end: step['end_point']
+    };
+  });
+  return ss;
+}
+
+function postRouteSteps(steps) {
+  $.ajax({
+    type: "POST",
+    contentType: "application/json; charset=utf-8",
+    url: 'commutes',
+    data: steps,
+    dataType: 'json',
+    success: function(m) {
+      console.log(m);
+    },
+    error: function (result) {
+      error();
+    }
+  });
+}
+
--- app/controllers/commutes_controller.rb
+++ app/controllers/commutes_controller.rb
@@ -4,4 +4,20 @@ class CommutesController < ApplicationController
 
   end
 
+  def create
+    respond_to do |format|
+
+      format.json do
+        lines = params[:_json]
+        lines.each do |line|
+          start = line['start']
+          terminus = line['end']
+          RouteStep.create(geom: "LINESTRING(#{start['H']} #{start['L']}, #{terminus['H']} #{terminus['L']})")
+        end
+      end
+
+    end
+  end
+
 end
+
